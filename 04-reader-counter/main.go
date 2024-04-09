2fe40b70bd813c5944f8276628e735e130f320cd
Ilia Egorychev <ilegorro@gmail.com>
2024-04-09 14:08:53 +0300

    Add 04-reader-counter

---
+++ 04-reader-counter/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+// nextFunc возвращает следующее слово из генератора
+type nextFunc func() string
+
+// counter хранит количество цифр в каждом слове.
+// ключ карты - слово, а значение - количество цифр в слове.
+type counter map[string]int
+
+// pair хранит слово и количество цифр в нем
+type pair struct {
+	word  string
+	count int
+}
+
+// countDigitsInWords считает количество цифр в словах,
+// выбирая очередные слова с помощью next()
+func countDigitsInWords(next nextFunc) counter {
+	pending := make(chan string)
+	counted := make(chan pair)
+	stats := counter{}
+
+	// отправляет слова на подсчет
+	go func() {
+		for w := next(); ; w = next() {
+			pending <- w
+			if w == "" {
+				return
+			}
+		}
+	}()
+
+	// считает цифры в словах
+	go func() {
+		for w := <-pending; ; w = <-pending {
+			counted <- pair{w, countDigits(w)}
+			if w == "" {
+				return
+			}
+		}
+	}()
+
+	for p := <-counted; p.word != ""; p = <-counted {
+		stats[p.word] = p.count
+	}
+
+	return stats
+}
+
+// countDigits возвращает количество цифр в строке
+func countDigits(str string) int {
+	count := 0
+	for _, char := range str {
+		if unicode.IsDigit(char) {
+			count++
+		}
+	}
+	return count
+}
+
+// printStats печатает слова и количество цифр в каждом
+func printStats(stats counter) {
+	for word, count := range stats {
+		fmt.Printf("%s: %d\n", word, count)
+	}
+}
+
+// wordGenerator возвращает генератор, который выдает слова из фразы
+func wordGenerator(phrase string) nextFunc {
+	words := strings.Fields(phrase)
+	idx := 0
+	return func() string {
+		if idx == len(words) {
+			return ""
+		}
+		word := words[idx]
+		idx++
+		return word
+	}
+}
+
+func main() {
+	phrase := "0ne 1wo thr33 4068"
+	next := wordGenerator(phrase)
+	stats := countDigitsInWords(next)
+	printStats(stats)
+}
