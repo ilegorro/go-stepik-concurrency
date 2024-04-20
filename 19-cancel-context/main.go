349e0b19c62c071b1df7017c567828f377026164
Ilia Egorychev <ilegorro@gmail.com>
2024-04-20 07:23:46 +0300

    Add 19-cancel-context

---
+++ 19-cancel-context/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+// генерит целые числа от start и до бесконечности
+func generate(ctx context.Context, start int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for i := start; ; i++ {
+			select {
+			case out <- i:
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return out
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	generated := generate(ctx, 11)
+	for num := range generated {
+		fmt.Print(num, " ")
+		if num > 14 {
+			break
+		}
+	}
+	fmt.Println()
+}
